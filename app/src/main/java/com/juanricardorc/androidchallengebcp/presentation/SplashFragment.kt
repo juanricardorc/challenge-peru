2b0ea65d0630e9e127cfb37933dd59222028d553
Juan Ricardo <juanricardorc@gmail.com>
2021-04-11 15:32:39 -0500

    se agregó SplalshFragment

---
+++ app/src/main/java/com/juanricardorc/androidchallengebcp/presentation/SplashFragment.kt
@@ -0,0 +1,29 @@
+package com.juanricardorc.androidchallengebcp.presentation
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.juanricardorc.androidchallengebcp.R
+import com.juanricardorc.androidchallengebcp.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Handler().postDelayed(Runnable {
+            findNavController().navigate(R.id.action_SplashFragment_to_FirstFragment)
+        }, 2500)
+    }
+}
